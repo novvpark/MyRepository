567bcf0d89154549672b21fdc5fcf8cf5d249eb9
novvpark <park@localhost>
2025-12-14 20:43:45 +0900

    master update C

--- GitHW/src/mygit/C.java
+++ GitHW/src/mygit/C.java
@@ -3,6 +3,7 @@ package mygit;
 public class C {
     public static void main(String[] args) {
         System.out.println("start");
+        System.out.println("master updated C");
         A a = new A();
         B b = new B();
         System.out.println(a.who());
